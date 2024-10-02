8b1b5023bb2e4e434192fc732619d0f53685220b
Mammoman <daniel08seun@gmail.com>
2024-10-02 22:49:41 +0100

    first-react-project

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React  from "react"
+import {  useState, useEffect } from "react";
+import './App.css'
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = 'http://www.omdbapi.com?apikey=532341d2';
+    
+
+const App = () => {
+
+  const [movies, setMovies ] = useState([]);
+  const [searchTerm, setsearchTerm] = useState([]);
+  
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    };
+
+    
+    useEffect (() => {
+    searchMovies('Superman');
+    }, [])
+
+    return (
+      <div className="app">
+        <h1>React-Movie</h1>
+
+        <div className="search">
+          <input 
+            placeholder="Search For Movies" 
+            value={searchTerm}
+            onChange={(e) => setsearchTerm(e.target.value)}
+            />
+        
+        <img 
+        src={SearchIcon}
+        alt="search"
+        onClick={() => searchMovies(searchTerm)}
+        />
+
+        </div>
+
+        {
+          movies?.length > 0
+          ? (
+            <div className = "container">
+             {movies.map((movie) => (
+              <MovieCard movie = {movie} />
+             ))}
+            </div>
+          ) : (
+            <div className = "empty"> 
+             <h2>No Movies</h2>
+            </div>
+          )
+        }
+         </div>
+    )
+};
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import App from  './App'
+
+import ReactDOM from "react-dom/client";
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
